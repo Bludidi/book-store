6537aaa99b2fb6d192a6ca2a62bf1e47e4a50f4e
Bludidi <bludidi5@gmail.com>
2022-11-17 18:19:27 +0200

    add book.js and addBook.js components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
+import Books from './components/books';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Books />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/books.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Book from './book';
+import Addbook from './addBook';
+import './books.css';
+
+const Books = () => (
+  <>
+    <ul>
+
+      <Book bookData={
+          {
+            title: 'The Hunger Games',
+            category: 'Action',
+            author: 'Suzanne Collins',
+            percent: '64',
+            chapter: 'Chapter 17',
+          }
+          }
+      />
+
+      <Book bookData={
+          {
+            title: 'Dune',
+            category: 'Science Fiction',
+            author: 'Frank Herbert',
+            percent: '8',
+            chapter: 'Chapter 3: "A Lesson Learned',
+          }
+          }
+      />
+
+      <Book bookData={
+          {
+            title: 'Capital in the Twenty-First Century',
+            category: 'Economy',
+            author: 'Suzanne Collins',
+            percent: '0',
+            chapter: 'Introduction',
+          }
+          }
+      />
+
+    </ul>
+    <Addbook />
+  </>
+);
+
+export default Books;
