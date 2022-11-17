2990c5f3eb13c9878ccaadc8ad63d9d01ced95b6
Bludidi <bludidi5@gmail.com>
2022-11-17 20:16:28 +0200

    add button in the categories component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Books from './components/books';
+import Categories from './components/categories';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
       </Routes>
     </div>
   );
